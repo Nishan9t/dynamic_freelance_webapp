41d65723e3c011f1c1bd2f7e22e61bd697d33dcc
Nishan9t <pandeynisha9t@gmail.com>
2023-08-08 16:14:59 +0530

    server is created with routes and template engine HBS

--- src/app.js
+++ src/app.js
@@ -1,1 +1,22 @@
-console.log("project started");
+const express = require('express');
+const hbs = require('hbs');
+require('dotenv').config();
+const routes=require('./routes/main.js')
+
+const app=express();
+
+
+app.use('',routes);
+
+//template engine => hbs
+app.set("view engine","hbs");
+//second views is name of folder
+app.set("views","views");
+
+
+
+PORT=process.env.PORT;
+
+app.listen(PORT,()=>{
+    console.log(`server started on ${PORT}`);
+})
